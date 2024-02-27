ca9e928fb28547cbe8aee9c944b6c1a5324fafea
R-Seel02 <Ryanjseely@icloud.com>
2024-02-26 20:56:28 -0500

    Added error message

--- Mod5.java
+++ Mod5.java
@@ -2,12 +2,22 @@ import java.util.Scanner;
 
 public class Mod5 {
     public static void main(String args[]){
+        System.out.println("Would you like to see a Cat or Dog");
         Scanner scan = new Scanner(System.in) ;
         String aniC = scan.nextLine();
         System.out.println("You picked" + aniC);
 
-        if(aniC.equals("Cat")); 
-        System.out.println("Cat ASCII ART HERE");
+        if(aniC.equals("Cat")) {
+            System.out.println("Cat ASCII ART HERE");
+         } else if (aniC.equals("Dog")) { 
+            System.out.println("Dog ASCII ART HERE");
+            
+            
+        } else{
+            System.out.println("Enter Dog or Cat Please");
+        }
+        
+
 
 
     }
