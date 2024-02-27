d5c8db1a662fe77e0482d822cc365b0d8656c069
R-Seel02 <Ryanjseely@icloud.com>
2024-02-26 20:39:46 -0500

    Added Scanner for animal choice

--- Mod5.java
+++ Mod5.java
@@ -1,7 +1,14 @@
-
+import java.util.Scanner;
 
 public class Mod5 {
     public static void main(String args[]){
+        Scanner scan = new Scanner(System.in) ;
+        String aniC = scan.nextLine();
+        System.out.println("You picked" + aniC);
+
+        if(aniC.equals("Cat")); 
+        System.out.println("Cat ASCII ART HERE");
+
 
     }
 }
