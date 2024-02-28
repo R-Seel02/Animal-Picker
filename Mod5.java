decc9cd313f40afbbfb112412171f0fc6ee06a22
R-Seel02 <Ryanjseely@icloud.com>
2024-02-28 00:53:47 -0500

    added looping valid option

--- Mod5.java
+++ Mod5.java
@@ -2,10 +2,18 @@ import java.util.Scanner;
 
 public class Mod5 {
     public static void main(String args[]){
-        System.out.println("Would you like to see a Cat or Dog");
         Scanner scan = new Scanner(System.in) ;
-        String aniC = scan.nextLine();
+        String aniC;
+       
+
+
+
+       
+    do {
+        System.out.println("Would you like to see a Cat or Dog");
+       aniC = scan.nextLine().toLowerCase();
         System.out.println("You picked" + aniC);
+
 
         if(aniC.equals("Cat")) {
             System.out.println("Cat ASCII ART HERE");
@@ -14,11 +22,20 @@ public class Mod5 {
             
             
         } else{
-            System.out.println("Enter Dog or Cat Please");
+            System.out.println("INVALID" + "Enter Dog or Cat Please");
         }
+
+
+    } while (!aniC.equals("dog") && !aniC.equals("cat"));
+
+    scan.close();
+       
+
         
 
 
 
     }
+
+   
 }
